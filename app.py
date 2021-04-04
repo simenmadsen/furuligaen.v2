695b660974aaefa17fb4083c7e172dddaea49594
oljebarn <espen.saevig@icloud.com>
2021-04-04 22:58:29 +0200

    fixtures

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from numpy import fix
 import pandas as pd
 import requests
 from datetime import timedelta, datetime
@@ -985,6 +986,48 @@ def transfers():
     result = render_template('transfers.html', data = data, getManagerName = getManagerName)
 
     return result
+
+@app.route("/fixtures")
+def fixtures():
+    def getTeamInfo():
+        url4 = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+        r4 = requests.get(url4)
+        teams = r4.json()['teams']
+        return teams
+    
+    def checkGameweek():
+        url3 = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+        r3 = requests.get(url3)
+        json = r3.json()
+        gameweek_df = pd.DataFrame(json['events'])
+        iscurrent = gameweek_df[['id', 'is_current']]
+        currentGw = iscurrent.loc[(iscurrent.is_current == True)].iat[0,0]
+        return currentGw
+    
+    teams = getTeamInfo()
+    thisGw = checkGameweek()
+
+    def getTeamName(teamId):
+        for team in teams:
+            if team['id'] == teamId:
+                return team["short_name"]
+    
+    def getTeamLogo(teamId):
+        for team in teams:
+            if team['id'] == teamId:
+                return team['code']
+
+    def getFixtures():
+        url = 'https://fantasy.premierleague.com/api/fixtures/?event=' + str(thisGw)
+        fixtures = requests.get(url).json()
+        return fixtures;
+
+    result = render_template('fixtures.html', fixtures = getFixtures(), getTeamName = getTeamName, getTeamLogo = getTeamLogo)
+
+    return result
+      
+
+
 
 if __name__ == '__main__':
     app.debug = True
